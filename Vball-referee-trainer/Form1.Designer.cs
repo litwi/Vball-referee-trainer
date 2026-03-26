2d24efd4c560ad18df092baeed0219759ec8b60e
Guillermo <guille_vivas_94@hotmail.com>
2026-03-26 11:58:33 +0000

    Added buttons

--- Vball-referee-trainer/Form1.Designer.cs
+++ Vball-referee-trainer/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.GenerateButton = new Button();
+            CorrectButton = new Button();
+            IncorrectButton = new Button();
+            SuspendLayout();
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new Point(142, 357);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new Size(111, 39);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            // 
+            // CorrectButton
+            // 
+            CorrectButton.Location = new Point(316, 357);
+            CorrectButton.Name = "CorrectButton";
+            CorrectButton.Size = new Size(109, 39);
+            CorrectButton.TabIndex = 1;
+            CorrectButton.Text = "Correct";
+            CorrectButton.UseVisualStyleBackColor = true;
+            // 
+            // IncorrectButton
+            // 
+            IncorrectButton.Location = new Point(458, 357);
+            IncorrectButton.Name = "IncorrectButton";
+            IncorrectButton.Size = new Size(109, 39);
+            IncorrectButton.TabIndex = 2;
+            IncorrectButton.Text = "Incorrect";
+            IncorrectButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(IncorrectButton);
+            Controls.Add(CorrectButton);
+            Controls.Add(this.GenerateButton);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button GenerateButton;
+        private Button CorrectButton;
+        private Button IncorrectButton;
     }
 }
